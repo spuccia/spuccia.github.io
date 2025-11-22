97f39aa72b94a537815979b8d0e1cf70bffc32a8
spuccia <saveriopucciait@gmail.com>
2025-11-22 19:58:32 +0100

    Semplifica estrazione tag da inviare al counter

--- assets/js/page-counter.js
+++ assets/js/page-counter.js
@@ -10,14 +10,8 @@ document.addEventListener('DOMContentLoaded', function() {
         tag = 'home';
     } else if (currentPath.startsWith('/about')) {
         tag = 'about';
-    } else if (currentPath.includes('/posts/') || currentPath.match(/\/\d{4}\/\d{2}\/\d{2}\//)) {
-        // Extract post slug from path for blog posts
-        const pathSegments = currentPath.split('/');
-        const postSlug = pathSegments[pathSegments.length - 2] || pathSegments[pathSegments.length - 1];
-        tag = postSlug.replace('.html', '').replace('/', '');
     } else {
-        // For other pages, use the path as tag (cleaned)
-        tag = currentPath.replace(/^\/|\/$/g, '').replace(/\//g, '-') || 'unknown';
+        tag = postSlug.replace('.html', '').replace('/', '');
     }
     
     // API endpoint and payload
